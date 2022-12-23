3a3483ce7767915eb8e8da79c9c40de23df52c1e
Akilan Arasu <akilan.arasu@revature.com>
2022-12-23 19:59:40 +0530

    Saved progress on Lists 2022-12-23 19:59:40 +0530

--- Lists/src/main/java/ListExercise.java
+++ Lists/src/main/java/ListExercise.java
@@ -52,6 +52,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
